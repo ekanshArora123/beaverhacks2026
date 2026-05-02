28cf7b6b2be9368c3b88f3c725d0e8014378aa86
Ancorro <cleasbys@oregonstate.edu>
2026-05-02 15:28:35 -0700

    update config

--- AIBackend/config.py
+++ AIBackend/config.py
@@ -3,4 +3,5 @@ VISION_MODEL = "gemini-3-pro-image-preview"
 VOICE_MODEL = "gemini-2.5-flash-preview-tts"
 DEFAULT_VOICE_NAME = "charon"
 
-# gemini-3.1-pro-preview good, no audio/image gen, 
+# gemini-3.1-pro-preview good, no audio/image gen
+# gemini-3-pro-image-preview, take image and text -> image
