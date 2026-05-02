7bf2ab93b9df91feb0bcdb6f13d5ae5d582d0958
Ancorro <cleasbys@oregonstate.edu>
2026-05-02 14:20:26 -0700

    ai backend API 1

--- AIBackend/AIAPICall.py
+++ AIBackend/AIAPICall.py
@@ -1,18 +1,72 @@
+ code Code
+
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import mimetypes
+import os
 from google import genai
 from google.genai import types
-from PIL import Image
 
-client = genai.Client()
 
-prompt = ("Create a picture of a nano banana dish in a fancy restaurant with a Gemini theme")
-response = client.models.generate_content(
-    model="gemini-3.1-flash-image-preview",
-    contents=[prompt],
-)
+def save_binary_file(file_name, data):
+    f = open(file_name, "wb")
+    f.write(data)
+    f.close()
+    print(f"File saved to to: {file_name}")
 
-for part in response.parts:
-    if part.text is not None:
-        print(part.text)
-    elif part.inline_data is not None:
-        image = part.as_image()
-        image.save("generated_image.png")
+
+def generate():
+    client = genai.Client(
+        api_key=os.environ.get("GEMINI_API_KEY"),
+    )
+
+    model = "gemini-3-pro-image-preview"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""INSERT_INPUT_HERE"""),
+            ],
+        ),
+    ]
+    tools = [
+        types.Tool(googleSearch=types.GoogleSearch(
+        )),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        image_config = types.ImageConfig(
+            aspect_ratio="",
+            image_size="1K",
+            person_generation="",
+        ),
+        response_modalities=[
+            "IMAGE",
+            "TEXT",
+        ],
+        tools=tools,
+    )
+
+    file_index = 0
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        if (
+            chunk.parts is None
+        ):
+            continue
+        if chunk.parts[0].inline_data and chunk.parts[0].inline_data.data:
+            file_name = f"ENTER_FILE_NAME_{file_index}"
+            file_index += 1
+            inline_data = chunk.parts[0].inline_data
+            data_buffer = inline_data.data
+            file_extension = mimetypes.guess_extension(inline_data.mime_type)
+            save_binary_file(f"{file_name}{file_extension}", data_buffer)
+        else:
+            if text := chunk.text:
+                print(text)
+
+if __name__ == "__main__":
+    generate()
