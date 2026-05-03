1b02e3d797162fd043b09b324eb412a34856ad31
Ancorro <cleasbys@oregonstate.edu>
2026-05-03 13:46:24 -0700

    c

--- backend/ApiScripts/GeminiEndpoint/config.py
+++ backend/ApiScripts/GeminiEndpoint/config.py
@@ -1,7 +1,9 @@
+VOICE_TO_TEXT_MODEL = "gemini-3-flash-preview"
 TEXT_MODEL = "gemini-3.1-pro-preview"
 VISION_MODEL = "gemini-3-pro-image-preview"
 VOICE_MODEL = "gemini-2.5-flash-preview-tts"
 DEFAULT_VOICE_NAME = "charon"
+VOICE_TO_TEXT_MODEL = "gemini-3-flash-preview"
 
 # gemini-3.1-pro-preview good, no audio/image gen
 # gemini-3-pro-image-preview, take image and text -> image
